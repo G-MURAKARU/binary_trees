dfa9fbce5bba5e9748a71f687da8fee1f4abe97e
Chebet Kamara <chebetkamara@gmail.com>
2023-03-02 08:43:40 -0800

    Binary trees

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures depth of node in a tree
+ * @tree: pointer to node
+ *
+ * Return: depth of tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t tree_depth = 0;
+
+	if (tree->parent)
+		tree_depth += binary_tree_depth(tree->parent) + 1;
+
+	return (tree_depth);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,19 +17,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-	{
 		left_tree_height += binary_tree_height(tree->left) + 1;
-	}
 	if (tree->right)
-	{
 		right_tree_height += binary_tree_height(tree->right) + 1;
-	}
-
 	if (left_tree_height >= right_tree_height)
 	{
 		return (left_tree_height);
 	}
-	else{
+	else
+	{
 		return (right_tree_height);
 	}
 }
